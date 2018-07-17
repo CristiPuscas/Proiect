eeeb85825920d17c2ab3c9bea4541ff5ffed797c
PuscasCristi <puscas.cristi1995@gmail.com>
2018-07-17 10:20:57 +0300

    modificari pentru antrenare

---
+++ confii/doi.php
@@ -0,0 +1,48 @@
+<!DOCTYPR html>
+<html>
+
+<body>
+<h1>Primul meu program php</h1>
+
+<?php
+//$color = "blue";
+//echo 'haha <br>';
+//echo $color. "<br>";
+
+//$x=5/*+15*/+5;
+//echo $x.'<br>';
+
+//$a=15;
+//$b=5;
+
+//function myTest() {
+  //  global $a , $b;
+    //$a=$a + $b;
+
+//}
+//myTest();
+//echo $a.'<br>';
+
+ //function myTest2(){
+   //  $c = 11;
+     //echo '<p>Variabila c din functie este: $c</p>';
+ //}
+
+ //myTest2();
+//if ($show_separators == TRUE) {
+//    echo "<hr>\n";
+//}
+//echo strpos("hello word! la la","word");
+//define("cristi","Cristi e cel mai tare");
+//echo cristi;
+$x=5;
+$y=2;
+echo $x/$y."<br>";
+echo $x%$y;
+if  $x==#y
+
+?>
+
+</body>
+
+</html>
