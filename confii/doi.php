ec9d888c5fa9bfb2e176ac7f17c77a0cf112acb8
PuscasCristi <puscas.cristi1995@gmail.com>
2018-07-17 10:57:28 +0300

    facem

--- confii/doi.php
+++ confii/doi.php
@@ -35,13 +35,28 @@
 //echo strpos("hello word! la la","word");
 //define("cristi","Cristi e cel mai tare");
 //echo cristi;
-$x=5;
-$y=2;
-echo $x/$y."<br>";
-echo $x%$y."<br>";
+$x=4;
+$y=6;
+//echo $x/$y."<br>";
+//echo $x%$y;
+//if  $x==#y
 echo ++$x."<br>";
-print --$y;
-
+print --$y."<br>";
+$a="este un string? ";
+$b=" Da este";
+echo $a.$b."<br>";
+echo $a.$x."<br>";
+echo $a.=$a."<br>";
+if ($x==$y){
+    $este=true;
+    echo $este;
+}
+if ($a===$b){
+    $este=false;
+    echo $este;
+}else{
+echo 'false';
+}
 ?>
 
 </body>
